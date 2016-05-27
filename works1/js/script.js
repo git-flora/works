e9360accddda79088a36a16a803fafe8a1a18e24
git-flora <345447866@qq.com>
2016-05-27 18:35:54 +0800

    Update script.js

--- works1/js/script.js
+++ works1/js/script.js
@@ -19,7 +19,7 @@ $(function(){
 	}
 
 	var $oParent=$("#main");
-	var dataInt={"data":[{"src":"1.jpg"},{"src":"2.jpg"},{"src":"3.jpg"},{"src":"4.jpg"},{"src":"5.jpg"},{"src":"6.jpg"},{"src":"7.jpg"},{"src":"8.jpg"},{"src":"9.jpg"},{"src":"10.jpg"},{"src":"11.jpg"},{"src":"12.jpg"},{"src":"13.jpg"},{"src":"14.jpg"}]};
+	var dataInt={"data":[{"src":"4.jpg"},{"src":"5.jpg"},{"src":"8.jpg"},{"src":"10.jpg"},{"src":"14.jpg"},{"src":"15.jpg"},{"src":"18.jpg"},{"src":"8.jpg"},{"src":"5.jpg"},{"src":"4.jpg"},{"src":"15.jpg"},{"src":"18.jpg"},{"src":"14.jpg"},{"src":"4.jpg"}]};
 	var num=0;
 	$(window).on("scroll",function(){
 	if(scrollSlide() && num<3){
